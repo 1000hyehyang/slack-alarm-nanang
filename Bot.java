848c66104be55d1485e7856b756b8c0abecbe871
1000hyehyang <ducogus12@gmail.com>
2025-02-20 18:25:05 +0900

    feat : 모든 야구팀 설정

--- Bot.java
+++ Bot.java
@@ -9,7 +9,7 @@ public class Bot {
 
         String message = """
         {
-            "text": "⚾ 좋아하는 야구팀을 선택하세요!",
+            "text": "KBO 좋아하는 야구팀을 선택하세요!! 나낭이 생각은요...",
             "attachments": [
                 {
                     "text": "팀을 선택해주세요:",
@@ -18,9 +18,16 @@ public class Bot {
                     "color": "#3AA3E3",
                     "attachment_type": "default",
                     "actions": [
+                        { "name": "team", "text": "기아 타이거즈", "type": "button", "value": "기아 타이거즈" },
+                        { "name": "team", "text": "삼성 라이온즈", "type": "button", "value": "삼성 라이온즈" },
                         { "name": "team", "text": "LG 트윈스", "type": "button", "value": "LG 트윈스" },
                         { "name": "team", "text": "두산 베어스", "type": "button", "value": "두산 베어스" },
-                        { "name": "team", "text": "SSG 랜더스", "type": "button", "value": "SSG 랜더스" }
+                        { "name": "team", "text": "KT 위즈", "type": "button", "value": "KT 위즈" },
+                        { "name": "team", "text": "SSG 랜더스", "type": "button", "value": "SSG 랜더스" },
+                        { "name": "team", "text": "한화 이글스", "type": "button", "value": "한화 이글스" },
+                        { "name": "team", "text": "롯데 자이언츠", "type": "button", "value": "롯데 자이언츠" },
+                        { "name": "team", "text": "NC 다이노스", "type": "button", "value": "NC 다이노스" },
+                        { "name": "team", "text": "키움 히어로즈", "type": "button", "value": "키움 히어로즈" }
                     ]
                 }
             ]
